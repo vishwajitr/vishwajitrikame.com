2d0eb009771337e14d5b207d990f5a6db09d8572
vishwajit <vishwajitrikamme@gmail.com>
2022-06-19 15:36:03 +0530

    changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a3b2640287d5402b"; }
+/******/ 		__webpack_require__.h = function() { return "24f136b0daf6f799"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 import Head from 'next/head'
 import Link from 'next/link';
+import Typewriter from 'typewriter-effect';
+
+
+
 
 
 export default function Home() {
@@ -9,39 +13,111 @@ export default function Home() {
         <title>Vishwajit Rikame</title>
       </Head>
       <header className='header'>
-        <div className="rightMenu">
-          <ul>
-            <li>About Me</li>
-            <li>
-            <Link href="../public/Vishwajit%20Rikame_Frontend%20Lead.pdf">
-              Resume
-              </Link>
-              </li>
-            <li>Contact Me</li>
-          </ul>
+        <div className="container">
+          <div className="header-inner">
+            <div className="logo">
+              Vishwajit
+            </div>
+            <div className="rightMenu">
+              <ul>
+                <li>About</li>
+                <li>Projects</li>
+                <li>
+                  <Link href="../public/Vishwajit%20Rikame_Frontend%20Lead.pdf">
+                    Resume
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="connectwithme">
+              <a href="">let's chat</a>
+            </div>
+          </div>
         </div>
       </header>
       <section className='first-section'>
-        <div className='full-container'></div>
-        <h1>Vishwajit Rikame</h1>
-        <h4>Team Lead - Frontend Development (Media.net) <br/>Ex. Flarepath || Ex. Hogoworks Solutions || Ex. SAPS <br/>Freelance Web Developer | FrontEnd Developer | Wordpress Developer</h4>  
-        <div className='talks-about'>
-        Talks about 
-        <span>#frontend</span>
-        <span>#webdevelopment</span>
-        <span>#frontenddeveloper</span>
-        <span>#frontenddevelopment</span>
-        </div>
-        <div className='max-50p'>
-          <p>
-          Overall 10+ Years of experience in IT right from Junior Web Developer to Team Lead - Frontend Development.
+        <div className="">
+          <div className="innerfirst">
+            <div className="leftpart">
+              <div className='leftpart-inner'>
+                <div className="typermain">
+                  <h1 className='iam'>Hi! I Am</h1>
 
-Skilled Frontend Engineer with 4 years of experience Project Leading and Mentoring Team members.
-
-I am experienced in Frontend development, worked on React JS, PHP frameworks, Emailer.
-          </p>
+                  <Typewriter
+                    onInit={(typewriter) => {
+                      typewriter.typeString('Vishwajit Rikame')
+                        .pauseFor(300)
+                        .deleteChars(16)
+                        .typeString('Frontend Developer')
+                        .pauseFor(300)
+                        .deleteChars(20)
+                        .typeString('I am Creative.')
+                        .pauseFor(300)
+                        .deleteChars(14)
+                        .typeString('I Love Design.')
+                        .pauseFor(300)
+                        .deleteChars(14)
+                        .typeString('I Love to Develop.')
+                        .pauseFor(300)
+                        .deleteChars(18)
+                        .typeString('Frontend Developer')
+                        .callFunction(() => {
+                          console.log('String typed out!');
+                        })
+                        .pauseFor(2500)
+                        // .deleteAll()
+                        .callFunction(() => {
+                          console.log('All strings were deleted');
+                        })
+                        .start();
+                    }}
+                  />
+                </div>
+                {/* <h1 className='myname'>Vishwajit Rikame</h1> */}
+                <h4>Team Lead - Frontend Development (Media.net) <br />Ex. Flarepath || Ex. Hogoworks Solutions || Ex. SAPS <br />Freelance Web Developer | FrontEnd Developer | Wordpress Developer</h4>
+                <div className='talks-about'>
+                  Talks about
+                  <span>#frontend</span>
+                  <span>#webdevelopment</span>
+                  <span>#frontenddeveloper</span>
+                  <span>#frontenddevelopment</span>
+                </div>
+                <div className='max-50p'>
+                  <p>
+                    Having more than 10 years of Web Developement experience<br />
+                    I am experienced in Frontend development, worked on React JS, PHP frameworks, Emailer.
+                  </p>
+                </div>
+              </div>
+              <div className='leftfooter'>
+                <div className="leftpart-inner">
+                  <div className='social-wrapper'>
+                    <ul>
+                      <li>
+                        <a href="https://www.linkedin.com/in/vishwajitrikame/" target="_blank">
+                          <img src="/linkedin-50-w.svg" alt="linkedin" />
+                        </a>
+                      </li>
+                      <li>
+                      <a href="https://github.com/vishwajitr/" target="_blank">
+                          <img src="/github-50-w.svg" alt="linkedin" />
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                  <div className='copyright'>
+                  vishwajitrikame&copy;2022
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="rightpart">
+            </div>
+          </div>
         </div>
       </section>
+
+
 
       <footer>
 
